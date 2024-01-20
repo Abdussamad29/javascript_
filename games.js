5180930bf4d2c26631aad09b18b8f2d6d5f1206b
Abdussamad <khanabdussamad32@gmail.com>
2024-01-20 23:43:08 +0530

    Delete games.js

--- games.js
+++
@@ -1,6 +0,0 @@
-// let choices = ['rock','paper','scissor']
-//           let random = Math.floor(Math.random()*choices.length)
-//           console.log(random);
-let choices = ['rock', 'paper', 'scissors'];
-let random = Math.floor(Math.random() * choices.length);
-console.log(random);
